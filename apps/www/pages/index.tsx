ae135dd788224012a9513f51a11862fc8b2c3d1e
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-02-05 12:12:57 -0500

    Update index.tsx

--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>tldraw</title>
+        <title>Iftiin-Board</title>
       </Head>
       <Editor id="home" showUI={!isExportMode} />
     </>
